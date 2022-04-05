6af6cd82921b2f44d7f027710a21cf46fcacf835
sophiacontesini <sophiamarques214@gmail.com>
2022-04-05 18:54:34 -0300

    add req 3

---
+++ src/pages/Config.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+export default class Config extends Component {
+  render() {
+    return (
+      <div>
+        <h1
+          className="title-config"
+          data-testid="settings-title"
+        >
+          Configurações
+
+        </h1>
+      </div>
+    );
+  }
+}
