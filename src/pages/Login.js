ab0c54cdd06b73f94bfd714ca164445272acab9b
Lindsey Fontana <lindseyfontanas@gmail.com>
2022-04-05 16:57:08 -0300

    Requisito 1 concluído

---
+++ src/pages/Login.js
@@ -0,0 +1,70 @@
+import React from 'react';
+
+class Login extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      name: '',
+      email: '',
+      buttonDisabled: true,
+    };
+  }
+
+   handleChange = ({ target }) => {
+     const { name, value } = target;
+     this.setState({
+       [name]: value,
+     });
+     this.isButtonDisabled();
+   }
+
+   isButtonDisabled = () => {
+     const { name, email } = this.state;
+     this.setState({
+       buttonDisabled: (name === '' && email === ''),
+     });
+   }
+
+   render() {
+     const { buttonDisabled, name, email } = this.state;
+     return (
+       <form>
+         <label htmlFor="name">
+           Nome do Jogador:
+           <input
+             type="text"
+             id="name"
+             name="name"
+             value={ name }
+             data-testid="input-player-name"
+             onChange={ this.handleChange }
+           />
+         </label>
+
+         <label htmlFor="email">
+           Email do Gravatar:
+           <input
+             type="email"
+             id="email"
+             name="email"
+             value={ email }
+             data-testid="input-gravatar-email"
+             onChange={ this.handleChange }
+           />
+         </label>
+         <button
+           type="submit"
+           data-testid="btn-play"
+           disabled={ buttonDisabled }
+         >
+           Play
+
+         </button>
+       </form>
+
+     );
+   }
+}
+
+export default Login;
