1a5507f7001696929c712130c9792b451172f3d0
imarmendes <imarmendes@gmail.com>
2022-04-07 11:07:07 -0300

    Requisito 13, testado e aprovado

--- src/pages/Feedback.js
+++ src/pages/Feedback.js
@@ -1,16 +1,35 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import Header from './components/Header';
 
+const THREE = 3;
+
 class Feedback extends Component {
   render() {
+    const { assertions } = this.props;
+    console.log(assertions);
     return (
       <>
         <Header />
         <h1 data-testid="feedback-text">Seu Feedback</h1>
+        <p data-testid="feedback-text">
+          {
+            assertions >= THREE ? 'Well Done!' : 'Could be better...'
+          }
+        </p>
       </>
     );
   }
 }
 
-export default connect()(Feedback);
+const mapStateToProps = (state) => ({
+  assertions: state.player.assertions,
+  // score: state.score,
+});
+
+Feedback.propTypes = {
+  assertions: PropTypes.string.isRequired,
+};
+
+export default connect(mapStateToProps)(Feedback);
